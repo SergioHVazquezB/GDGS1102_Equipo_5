e259836ce08f8a03febc692cf55f5f54115e4eb3
Jesús Gómez <gomezsanchezjesys@gmail.com>
2021-12-16 19:01:19 -0600

    App PWA Concluida

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module_exports = {
+    pwa: {
+        name: 'app_pwa',
+        themeColor: '#fff',
+        msTileColor: '#000',
+        workboxPluginMode: 'GenerateSW',
+        workboxOptions: {
+            skepWaiting: true,
+        }
+    }
+}
